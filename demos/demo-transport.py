e2c28d6abef1d91fea1e0e2c5db5384167921062
Allan Leal <allan.leal@erdw.ethz.ch>
2019-09-12 10:23:19 +0200

    Add demo for TransportSolver

---
+++ demos/demo-transport.py
@@ -0,0 +1,46 @@
+import firedrake as fire
+import rok
+
+nx = 25  # the number of mesh cells along the x-coordinate
+ny = 25  # the number of mesh cells along the y-coordinate
+
+nsteps = 100  # the number of time steps
+
+D  = fire.Constant(1.0e-9)           # the diffusion coefficient (in units of m2/s)
+v  = fire.Constant([1.0e-6, 0.0])    # the fluid pore velocity (in units of m/s)
+q  = fire.Constant(0.0)              # the source rate (in units of [u]/s)
+dt = 2.0e5                             # the time step (in units of s)
+
+# Initialise the mesh
+mesh = fire.UnitSquareMesh(nx, ny, quadrilateral=True)
+
+V = fire.FunctionSpace(mesh, 'CG', 1)
+
+bc = fire.DirichletBC(V, fire.Constant(1.0), 1)
+
+u = fire.Function(V)
+
+# Initialize the transport solver
+transport = rok.TransportSolver()
+transport.setVelocity(v)
+transport.setDiffusion(D)
+transport.setSource(q)
+transport.setBoundaryConditions([bc])
+
+t = 0.0
+step = 0
+
+outfile = fire.File("u.pvd")
+outfile.write(u)
+
+while step < nsteps:
+
+    transport.step(u, dt)
+
+    t += dt
+    step += 1
+
+    if step % 1 == 0:
+        outfile.write(u)
+        print("t = {:<15.3f} step = {:<15}".format(t, step))
+
