7667c47021be7fcc545c36b4721a7464f15db300
Allan Leal <allan.leal@erdw.ethz.ch>
2019-09-13 14:53:47 +0200

    Implement a demo using DarcySolver

---
+++ demos/demo-darcysolver.py
@@ -0,0 +1,26 @@
+import rok
+
+
+rho = rok.Constant(1000.0)  # water density (in units of kg/m3)
+mu = rok.Constant(8.9e-4)  # water viscosity (in units of Pa*s)
+k = rok.Constant(1e-12)  # rock permeability (in units of m2)
+f = rok.Constant(0.0)
+
+mesh = rok.UnitSquareMesh(32, 32, quadrilateral=True)
+
+problem = rok.DarcyProblem(mesh)
+problem.setFluidDensity(rho)
+problem.setFluidViscosity(mu)
+problem.setRockPermeability(k)
+problem.setSourceRate(f)
+problem.addPressureBC(100e5, 'left')
+problem.addPressureBC(1e5, 'right')
+problem.addVelocityComponentBC(0.0, 'y', 'bottom')
+problem.addVelocityComponentBC(0.0, 'y', 'top')
+
+solver = rok.DarcySolver(problem)
+solver.solve()
+
+file = rok.File('demo-darcysolver.pvd')
+
+file.write(solver.u, solver.p)
