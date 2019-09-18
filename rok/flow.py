cef4898a81345296607cb6504b960ebaebfa76e9
volpatto <volpatto@lncc.br>
2019-09-18 02:17:36 +0100

    Separate the form constructor from DarcySolver

--- rok/flow.py
+++ rok/flow.py
@@ -14,34 +14,26 @@ class DarcyProblem:
         self.bcs_p = []
         self.bcs_u = []
 
-
     def setFluidViscosity(self, mu):
         self.mu = mu
-
 
     def setFluidDensity(self, rho):
         self.rho = rho
 
-
     def setRockPermeability(self, k):
         self.k = k
-
 
     def setSourceRate(self, f):
         self.f = f
 
-
     def addImpermeableBoundary(self, index):
         pass
-
 
     def addPressureBC(self, value, onboundary):
         self.bcs_p.append((value, boundaryNameToIndex(onboundary)))
 
-
     def addVelocityBC(self, value, onboundary):
         self.bcs_u.append((value, boundaryNameToIndex(onboundary), None))
-
 
     def addVelocityComponentBC(self, value, component, onboundary):
         self.bcs_u.append((value, boundaryNameToIndex(onboundary), vectorComponentNameToIndex(component)))
@@ -49,27 +41,59 @@ class DarcyProblem:
 
 class DarcySolver:
 
-    def __init__(self, problem):
+    def __init__(self, problem, u_degree=1, p_degree=1):
         self.problem = problem
-
         mesh = problem.mesh
+        bcs_p = problem.bcs_p
+        bcs_u = problem.bcs_u
+
+        self._U = fire.VectorFunctionSpace(mesh, 'CG', u_degree)
+        self._V = fire.FunctionSpace(mesh, 'CG', p_degree)
+        self._W = self._U * self._V
+
+        self._a, self._L = self.cgls_form(problem, mesh, bcs_p)
+
+        self.solution = fire.Function(self._W)
+        self.u = fire.Function(self._U, name='u')
+        self.p = fire.Function(self._V, name='p')
+
+        # Construct the Dirichlet boundary conditions for velocity
+        self.bcs = []
+        for uboundary, iboundary, component in bcs_u:
+            if component != None:
+                self.bcs.append(fire.DirichletBC(self._W.sub(0).sub(component), uboundary, iboundary))
+            else:
+                self.bcs.append(fire.DirichletBC(self._W.sub(0), uboundary, iboundary))
+
+        # solver_parameters = {
+        #     # This setup is suitable for 3D
+        #     'ksp_type': 'fgmres',
+        #     'pc_type': 'ilu',
+        #     'mat_type': 'aij',
+        #     'ksp_rtol': 1e-5,
+        #     'ksp_max_it': 2000,
+        #     'ksp_monitor': None
+        # }
+        self.solver_parameters = {
+            "mat_type": "aij",
+            "ksp_type": "preonly",
+            "pc_type": "lu",
+            "pc_factor_mat_solver_type": "mumps"
+        }
+
+        lvp = fire.LinearVariationalProblem(self._a, self._L, self.solution, bcs=self.bcs)
+
+        self.solver = fire.LinearVariationalSolver(lvp, solver_parameters=self.solver_parameters)
+
+    def cgls_form(self, problem, mesh, bcs_p):
+
         rho = problem.rho
         mu = problem.mu
         k = problem.k
         f = problem.f
-        bcs_p = problem.bcs_p
-        bcs_u = problem.bcs_u
 
-        U = fire.VectorFunctionSpace(mesh, 'CG', 1)
-        V = fire.FunctionSpace(mesh, 'CG', 1)
-        W = U * V
-
-        self.solution = fire.Function(W)
-        self.u = fire.Function(U, name='u')
-        self.p = fire.Function(V, name='p')
-
-        q, p = fire.TrialFunctions(W)
-        w, v = fire.TestFunctions(W)
+        q, p = fire.TrialFunctions(self._W)
+        w, v = fire.TestFunctions(self._W)
 
         n = fire.FacetNormal(mesh)
 
@@ -77,9 +101,9 @@ class DarcySolver:
         h = fire.CellDiameter(mesh)
         has_mesh_characteristic_length = False
         delta_0 = fire.Constant(-1)
-        delta_1 = fire.Constant(1/2)
-        delta_2 = fire.Constant(1/2)
-        delta_3 = fire.Constant(1/2)
+        delta_1 = fire.Constant(1 / 2)
+        delta_2 = fire.Constant(1 / 2)
+        delta_3 = fire.Constant(1 / 2)
 
         # Some good stabilizing methods that I use:
         # 1) CLGS (Correa and Loula method, it's a Galerkin Least-Squares residual formulation):
@@ -114,50 +138,23 @@ class DarcySolver:
             delta_3 = delta_3 * h * h
 
         kappa = rho * k / mu
-        inv_kappa = 1.0/kappa
+        inv_kappa = 1.0 / kappa
 
         # Classical mixed terms
-        self.a = (dot(inv_kappa * q, w) - div(w) * p - delta_0 * v * div(q)) * dx
-        self.L = -delta_0 * f * v * dx
+        a = (dot(inv_kappa * q, w) - div(w) * p - delta_0 * v * div(q)) * dx
+        L = -delta_0 * f * v * dx
 
         # Add the contributions of the pressure boundary conditions to L
         for pboundary, iboundary in bcs_p:
-            self.L -= pboundary * dot(w, n) * ds(iboundary)
+            L -= pboundary * dot(w, n) * ds(iboundary)
 
         # Stabilizing terms
-        self.a += delta_1 * inner(kappa * (inv_kappa * q + grad(p)), delta_0 * inv_kappa * w + grad(v)) * dx
-        self.a += delta_2 * inv_kappa * div(q) * div(w) * dx
-        self.a += delta_3 * inner(kappa * curl(inv_kappa * q), curl(inv_kappa * w)) * dx
-        self.L += delta_2 * inv_kappa * f * div(w) * dx
+        a += delta_1 * inner(kappa * (inv_kappa * q + grad(p)), delta_0 * inv_kappa * w + grad(v)) * dx
+        a += delta_2 * inv_kappa * div(q) * div(w) * dx
+        a += delta_3 * inner(kappa * curl(inv_kappa * q), curl(inv_kappa * w)) * dx
+        L += delta_2 * inv_kappa * f * div(w) * dx
 
-        # Construct the Dirichlet boundary conditions for velocity
-        self.bcs = []
-        for uboundary, iboundary, component in bcs_u:
-            if component != None:
-                self.bcs.append(fire.DirichletBC(W.sub(0).sub(component), uboundary, iboundary))
-            else:
-                self.bcs.append(fire.DirichletBC(W.sub(0), uboundary, iboundary))
-
-        # solver_parameters = {
-        #     # This setup is suitable for 3D
-        #     'ksp_type': 'fgmres',
-        #     'pc_type': 'ilu',
-        #     'mat_type': 'aij',
-        #     'ksp_rtol': 1e-5,
-        #     'ksp_max_it': 2000,
-        #     'ksp_monitor': None
-        # }
-        self.solver_parameters = {
-            "mat_type": "aij",
-            "ksp_type": "preonly",
-            "pc_type": "lu",
-            "pc_factor_mat_solver_type": "mumps"
-        }
-
-        lvp = fire.LinearVariationalProblem(self.a, self.L, self.solution, bcs=self.bcs)
-
-        self.solver = fire.LinearVariationalSolver(lvp, solver_parameters=self.solver_parameters)
-
+        return a, L
 
     def solve(self):
         self.solver.solve()
