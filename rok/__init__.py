c24cd7958b040f6ae4e41ace6bb479151b0856a1
Allan Leal <allanmulin@gmail.com>
2019-09-18 11:28:55 +0200

    Export permeability module in rok

--- rok/__init__.py
+++ rok/__init__.py
@@ -2,6 +2,7 @@ from .flow import DarcyProblem, DarcySolver
 from .transport import TransportSolver
 from .chemicaltransport import ChemicalDirichletBC, ChemicalTransportSolver
 from .chemicalfield import ChemicalField
+from .permeability import permeability
 from .utils import DirichletExpressionBC
 
 from firedrake import *
