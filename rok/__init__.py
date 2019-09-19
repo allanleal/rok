d8285f140ea0b28080180e4417180e89b75cb759
Allan Leal <allan.leal@erdw.ethz.ch>
2019-09-19 15:41:54 +0200

    Import reaktoro

--- rok/__init__.py
+++ rok/__init__.py
@@ -1,8 +1,11 @@
+from firedrake import *
+from reaktoro import *
+
+del globals()['ChemicalField']  # Reaktoro also has a ChemicalField class, different from the one in Rok
+
 from .flow import DarcyProblem, DarcySolver
 from .transport import TransportSolver
 from .chemicaltransport import ChemicalDirichletBC, ChemicalTransportSolver
 from .chemicalfield import ChemicalField
 from .permeability import permeability
 from .utils import DirichletExpressionBC
-
-from firedrake import *
