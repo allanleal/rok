1414faa81a3221d456c6c08b62e9f3017ed0eb32
Allan Leal <allan.leal@erdw.ethz.ch>
2019-10-21 10:54:08 +0200

    Improve test organization (move regression dir to upper dir)

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,43 +1,90 @@
+import numpy as np
 import rok
 
 
-def test_DarcyVelocityBC():
-    mesh = rok.UnitSquareMesh(500, 500, quadrilateral=True)
-    V = rok.VectorFunctionSpace(mesh, 'CG', 1)
-    U = rok.FunctionSpace(mesh, 'CG', 1)
-    x, y = rok.SpatialCoordinate(mesh)
-
-    rho = rok.Function(U)
-    rho.assign(rok.Constant(2.0))
-
-    ubc = rok.DarcyVelocityBC(V, rok.Constant([6.0, -3.0]), 1)
-
-    ubc.update(rho)
-
-    boundarydofs = V.boundary_nodes(1, "topological")
-
-    assert (ubc.qboundary.dat.data[boundarydofs, 0] == 12.0).all()
-    assert (ubc.qboundary.dat.data[boundarydofs, 1] == -6.0).all()
+rho = rok.Constant(1000.0)  # water density (in units of kg/m3)
+mu = rok.Constant(8.9e-4)  # water viscosity (in units of Pa*s)
+k = rok.Constant(1e-12)  # rock permeability (in units of m2)
+f = rok.Constant(0.0)
 
 
-def test_DirichletExpressionBC():
-    mesh = rok.UnitSquareMesh(500, 500, quadrilateral=True)
-    V = rok.VectorFunctionSpace(mesh, 'CG', 1)
-    U = rok.FunctionSpace(mesh, 'CG', 1)
-    x, y = rok.SpatialCoordinate(mesh)
+def test_darcysolver_problem1(num_regression):
 
-    rho = rok.Function(U)
+    mesh = rok.UnitSquareMesh(5, 5, quadrilateral=True)
 
-    ubc = rok.DirichletExpressionBC(V, rho * rok.Constant([6.0, -3.0]), 1)
+    problem = rok.DarcyProblem(mesh)
+    problem.setFluidDensity(rho)
+    problem.setFluidViscosity(mu)
+    problem.setRockPermeability(k)
+    problem.setSourceRate(f)
+    problem.addPressureBC(100e5, 'left')
+    problem.addPressureBC(1e5, 'right')
+    problem.addVelocityComponentBC(0.0, 'y', 'bottom')
+    problem.addVelocityComponentBC(0.0, 'y', 'top')
 
-    rho.assign(rok.Constant(2.0))
+    solver = rok.DarcySolver(problem, method='cgls')
+    solver.solve()
 
-    ubc.update()
+    solver.u.dat.data, solver.p.dat.data
 
-    boundarydofs = V.boundary_nodes(1, "topological")
+    data = {}
+    data['ux'] = solver.u.dat.data[:, 0]
+    data['uy'] = solver.u.dat.data[:, 1]
+    data['p'] = solver.p.dat.data
 
-    assert (ubc.uboundary.dat.data[boundarydofs, 0] == 12.0).all()
-    assert (ubc.uboundary.dat.data[boundarydofs, 1] == -6.0).all()
+    num_regression.check(data)
 
-# test_DarcyVelocityBC()
-test_DirichletExpressionBC()
+
+def test_transport_step_dgls(num_regression):
+
+    mesh = rok.UnitSquareMesh(5, 5, quadrilateral=True)
+
+    problem = rok.DarcyProblem(mesh)
+    problem.setFluidDensity(rho)
+    problem.setFluidViscosity(mu)
+    problem.setRockPermeability(k)
+    problem.setSourceRate(f)
+    problem.addPressureBC(100e5, 'left')
+    problem.addPressureBC(1e5, 'right')
+    problem.addVelocityComponentBC(0.0, 'y', 'bottom')
+    problem.addVelocityComponentBC(0.0, 'y', 'top')
+
+    solver = rok.DarcySolver(problem, method='dgls')
+    solver.solve()
+
+    solver.u.dat.data, solver.p.dat.data
+
+    data = {}
+    data['ux'] = solver.u.dat.data[:, 0]
+    data['uy'] = solver.u.dat.data[:, 1]
+    data['p'] = solver.p.dat.data
+
+    num_regression.check(data)
+
+
+def test_darcysolver_problem2(num_regression):
+
+
+    mesh = rok.UnitSquareMesh(5, 5, quadrilateral=True)
+
+    problem = rok.DarcyProblem(mesh)
+    problem.setFluidDensity(rho)
+    problem.setFluidViscosity(mu)
+    problem.setRockPermeability(k)
+    problem.setSourceRate(f)
+    problem.addPressureBC(1e5, 'right')
+    problem.addVelocityBC([1e-5, 0.0], 'left')
+    problem.addVelocityComponentBC(0.0, 'y', 'bottom')
+    problem.addVelocityComponentBC(0.0, 'y', 'top')
+
+    solver = rok.DarcySolver(problem)
+    solver.solve()
+
+    solver.u.dat.data, solver.p.dat.data
+
+    data = {}
+    data['ux'] = solver.u.dat.data[:, 0]
+    data['uy'] = solver.u.dat.data[:, 1]
+    data['p'] = solver.p.dat.data
+
+    num_regression.check(data)
